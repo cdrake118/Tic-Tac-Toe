331d98f5f5a356e13bab27b29235648ab7b4af5e
cdrake118 <cdrake@gmail.com>
2020-05-09 21:59:38 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 The boardgame.io Authors.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+import React from "react";
+import { render } from "react-dom";
+import { Client } from "boardgame.io/react";
+import { SocketIO } from "boardgame.io/multiplayer";
+import { TicTacToe } from "./game";
+import { TicTacToeBoard } from "./board";
+
+const TicTacToeClient = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+  debug: false,
+  multiplayer: SocketIO({ server: "localhost:8000" })
+});
+
+class App extends React.Component {
+  state = { playerID: null };
+
+  render() {
+    if (this.state.playerID === null) {
+      return (
+        <div>
+          <p>Play as</p>
+          <button onClick={() => this.setState({ playerID: "0" })}>
+            Player 0
+          </button>
+          <button onClick={() => this.setState({ playerID: "1" })}>
+            Player 1
+          </button>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <TicTacToeClient playerID={this.state.playerID} />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
